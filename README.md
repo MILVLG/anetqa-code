f7d89fa38aaa26242abdbc3ec008bdb3216814bb
zlxhhh <942370913@qq.com>
2023-04-12 13:41:20 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Code for ANetQA baselines
 
-This repository contains code for our baselines HCRN, ClipBERT, and all-in-one. For all three baselines, we used their code for the FrameQA questions in the TGIF-QA benchmark, then adjusted to fit our data structure. You can download ANetQA from our [website](https://milvlg.github.io/anetqa).
+This repository contains code for our baselines hcrn, clipbert, and all-in-one. For all three baselines, we used their code for the FrameQA questions in the TGIF-QA benchmark, then adjusted to fit our data structure. You can download ANetQA from our [website](https://milvlg.github.io/anetqa).
 
 ## Prepare dataset
 
@@ -8,7 +8,7 @@ see [dataset](https://github.com/MILVLG/anetqa-code/tree/main/dataset)
 
 ## Models
 
-see [hcrn](https://github.com/MILVLG/anetqa-code/tree/main/hcrn), [ClipBert](https://github.com/MILVLG/anetqa-code/tree/main/ClipBERT), [all-in-one](https://github.com/MILVLG/anetqa-code/tree/main/all-in-one)
+see [hcrn](https://github.com/MILVLG/anetqa-code/tree/main/hcrn), [clipbert](https://github.com/MILVLG/anetqa-code/tree/main/ClipBERT), [all-in-one](https://github.com/MILVLG/anetqa-code/tree/main/all-in-one)
 
 ## Results
 
---
+++ hcrn/eval_res.py
@@ -0,0 +1,48 @@
+import json
+from tqdm import tqdm
+
+val_qas = json.load(open("release_data/anetqa/anetqa_val.json"))
+res_path=""
+res = json.load(open(f"{res_path}"))
+
+structures={}
+semantics={}
+reasoning={}
+ans_type={}
+val_gt={}
+for qa in tqdm(val_qas):
+    for i in qa["taxonomy"]["question structures"]:
+        if i not in structures:
+            structures[i]=[]
+        structures[i].append(qa["qa_id"])
+    for i in qa["taxonomy"]["question semantics"]:
+        if i not in semantics:
+            semantics[i]=[]
+        semantics[i].append(qa["qa_id"])
+    for i in qa["taxonomy"]["reasoning skills"]:
+        if i not in reasoning :
+            reasoning [i]=[]
+        reasoning [i].append(qa["qa_id"])
+    for i in qa["taxonomy"]["answer types"]:
+        if i not in ans_type:
+            ans_type[i]=[]
+        ans_type[i].append(qa["qa_id"])
+    val_gt[qa["qa_id"]]=qa["answer"]
+
+ans={}
+for i in res:
+    qa_id = i["question_id"]
+    pred = i["pred"]
+    gt = val_gt[qa_id]
+    if pred==gt:
+        ans[qa_id]=1
+    else:
+        ans[qa_id]=0
+
+taxs = [structures,semantics,reasoning,ans_type]
+for tax in taxs:
+    for i in tax:
+        count=0
+        for j in tax[i]:
+            count+=ans[j]
+        print(i,count/len(tax[i]))
